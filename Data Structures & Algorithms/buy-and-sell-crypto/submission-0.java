7ca660f210befd541d959bc321caac2fc792b1e4
Vikash Prasad <118461836+Vikash0277@users.noreply.github.com>
2026-07-09 21:55:10 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxPro = 0;
+        for(int i = 0; i < prices.length - 1; i++) {
+            for(int j = i+ 1; j < prices.length; j++) {
+                int calculateProfit = prices[j] - prices[i];
+                maxPro = Math.max(maxPro, calculateProfit);
+            }
+        }
+        return maxPro;
+    }
+}
